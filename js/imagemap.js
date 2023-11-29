dc6911983f5144ea7af3d54719c6a4e38990ac76
Emily <emv38@pitt.edu>
2023-11-29 12:55:55 -0500

    reveal on scroll for containers with reveal in them

--- js/imagemap.js
+++ js/imagemap.js
@@ -33,3 +33,22 @@ function showContainer(containerId) {
     const selectedContainer = document.getElementById(containerId);
     selectedContainer.style.display = 'block';
 }
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i =0 ; i< reveals.length ; i++){
+        var windowheight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealTop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
